b4ece15c4105da87decae1790593fa59280d9b34
JayavardhanV <37415378+JayavardhanV@users.noreply.github.com>
2020-06-23 13:09:29 -0400

    Add files via upload
    Updated version added

--- get_posts.py
+++ get_posts.py
@@ -6,11 +6,9 @@ import requests
 
 USAGE = "Usage: python {sys.argv[0]} [--help] | startIndex endIndex outputFileName]"
 responseTotal = {}
-@dataclasses.dataclass
-class Arguments:
-    start: int
-    end: int
-    out: str
+start: int 
+end: int 
+out: str 
 
 # getApiResponse
 def getAPIResponse():
@@ -25,34 +23,59 @@ def getAPIResponse():
         raise
 
 # Export data
-def exportData(args:Arguments):
-    filterList =[]
-    try:
-        for key in responseTotal:
-            if(int(key)>=int(args.start) and int(key)<=int(args.end)):
-                filterList.append(responseTotal[key])
+def exportData(start,end,out):
+    # if no endindex
+    if(end==0):
+        end = len(responseTotal)
 
-        with open(args.out, 'w') as f:
-            print(filterList, file=f)
+    # if Start index is greaterthan endindex
+    if(start>end):
+        temp = end
+        end=start
+        start =temp
+    
+    filterList =[]
+    try:        
+        for key in responseTotal:
+            if(int(key)>=int(start) and int(key)<=int(end)):
+                filterList.append(responseTotal[key])
+        
+        #no input file passed, then display response in terminal     
+        if(out == ''):
+            for key in filterList:
+                print(key)
+        else:
+            with open(out, 'w') as f:
+                print(filterList, file=f)
     except:
         print("Unexpected error:", sys.exc_info()[0])
         raise
 
 # move args to class object
 def validate(args: List[str]):
-    if len(args) > 2 and args[2].isdigit():
-        args[2] = int(args[2])
-    try:
-        arguments = Arguments(*args)
-    except TypeError:
-        raise SystemExit(USAGE)
-    
+    start = 0
+    end = 0
+    out =''
+    position =1
+
+    while(len(args)-1>=position):
+        if(position == 1):
+            start = args[1]
+        elif( position == 3):
+            end = args[3]
+        elif(position == 5):
+            out = args[5]
+        position = position + 1
+
+    #fetch api response
     getAPIResponse()
 
-    exportData(arguments)
+    #show data into terminal or dump data into file
+    exportData(start,end,out)
 
 def main() -> None:
     args = sys.argv[1:]
+    
     if not args:
         raise SystemExit(USAGE)
 
